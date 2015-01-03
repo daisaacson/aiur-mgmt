7635e7ae567efa4994a8e058b48779b9e17a640f
daisaacson <49328302+daisaacson@users.noreply.github.com>
2015-01-03 03:54:36 +0000

    patch.sh improvements

--- patch.sh
+++ patch.sh
@@ -1,63 +1,123 @@
 #!/usr/bin/env bash
 
-function printHelp ()
+# EXIT CODES
+#  0 - should have worked
+#  1 -
+#  2 - Yum clean failed
+#  4 - Yum list updates failed
+#  8 - Yum update failed
+
+function aiurpatchhelp ()
 {
-  echo "$0:";
-  echo " -h  html ouput";
+  echo "Usage: $0 [options]... [package]...";
+  echo "options:";
+  echo " -b  print <br> tags";
   echo " -r  reboot";
+}
+
+function aiurpatchprint ()
+{
+  if [[ $HTML -eq 1 ]]; then
+    echo "$@" | sed 's/$/<br>/gm';
+  else
+    echo "$@" 
+  fi;
+}
+
+function aiurpatchyum ()
+{
+  set -o pipefail;
+  # yum clean
+  if YUMCLEAN=$(yum --quiet clean all 2>&1); then
+    # yum list updates
+    if YUMLIST=$(yum --quiet list updates $@ 2>&1); then
+      # yum update
+      if YUMUPDATE=$(yum -y --quiet update $@ 2>&1); then
+        if [[ "$YUMLIST"  != "" ]]; then
+          aiurpatchprint "$YUMLIST";
+        else
+          aiurpatchprint "No updates available";
+        fi;
+        aiurpatchprint "yum update complete";
+        aiurpatchprint "OKAY"
+      # yum update failed
+      else
+        aiurpatchprint "$YUMLIST"
+        aiurpatchprint "$YUMUPDATE"
+        aiurpatchprint "yum update failed";
+        aiurpatchprint "FAILED"
+        RETVAL=$((RETVAL+8));
+      fi;
+    # yum list updates failed
+    else
+      aiurpatchprint "$YUMLIST";
+      aiurpatchprint "yum list updates $@ failed";
+      aiurpatchprint "FAILED"
+      RETVAL=$((RETVAL+4));
+    fi;
+  # yum clean failed
+  else
+    aiurpatchprint "$YUMCLEAN"
+    aiurpatchprint "yum clean failed";
+    aiurpatchprint "FAILED"
+    RETVAL=$((RETVAL+2));
+  fi
+}
+
+function aiurpatchup2date ()
+{
+  if UP2DATE=$(up2date -u 2>&1); then
+    aiurpatchprint "$UP2DATE";
+    aiurpatchprint "up2date -u complete"
+    aiurpatchprint "OKAY";
+  else
+    aiurpatchprint "$UP2DATE";
+    aiurpatchprint "up2date -u failed"
+    aiurpatchprint "FAILED"
+    RETVAL=$((RETVAL+8))
+  fi
+}
+
+# Detect if old, up2date, is used
+function detectup2date ()
+{
+  if $(which up2date >/dev/null 2>&1); then
+    return 0;
+  fi
+  return 1;
 }
 
 function main ()
 {
-  set -o pipefail;
-  if $(yum --quiet clean all >/dev/null 2>&1); then
-    if PKGS=$(yum --quiet list updates 2>/dev/null); then
-      if $(yum -y --quiet update >/dev/null 2>&1); then
-        if [[ "$PKGS"  != "" ]]; then
-          if [[ $HTML -eq 0 ]]; then
-            echo "$PKGS"
-          else
-            echo "$PKGS" | sed 's/$/<br>/gm';
-          fi;
-        else
-          if [[ $HTML -eq 0 ]]; then
-            echo "No updates available";
-          else
-            echo "No updates available<br>";
-          fi;
-        fi;
-        echo "Yum update complete";
-        if [[ $REBOOT -eq 1 ]]; then
-          reboot;
-        fi;
-      else
-        echo "Yum update failed";
-      fi;
-    else
-      echo "Yum list updates failed";
-    fi;
+  if detectup2date; then
+    aiurpatchup2date "$@";
   else
-    echo "Yum clean failed";
+    aiurpatchyum "$@";
+  fi
+  if [[ $REBOOT -eq 1 && $RETVAL -eq 0 ]]; then
+    reboot;
   fi
 }
 
 HTML=0;
 REBOOT=0;
+RETVAL=0;
 
-while getopts ":hr" opt; do
+while getopts ":br" opt; do
   case $opt in
-    h)
+    b)
       HTML=1;
       ;;
     r)
       REBOOT=1;
       ;;
     \?)
-      printHelp;
+      aiurpatchhelp;
       exit 1;
       ;;
   esac;
 done;
 shift $((OPTIND - 1));
 
-main
+main $@
+exit $RETVAL
