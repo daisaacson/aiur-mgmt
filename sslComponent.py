376c6e53e562589495613a0fdbef3406a7e888c6
daisaacson <49328302+daisaacson@users.noreply.github.com>
2025-08-01 21:46:01 -0500

    update for python3

--- sslComponent.py
+++ sslComponent.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 __version__ = 0.1
 
-import os, sys, re
+import os, sys, re, socket
 from datetime import datetime
 from optparse import OptionParser, OptionGroup
 from subprocess import Popen, PIPE, STDOUT
@@ -22,17 +22,17 @@ class Ssl:
     return self.getTimeToExpire() < other.getTimeToExpire()
   def getSubject(self):
     p = Popen(["openssl", "x509", "-noout", "-subject"], stdout=PIPE, stdin=PIPE)
-    return self.cleanOutput(p.communicate(input=self.x509)[0])
+    return self.cleanOutput(p.communicate(input=self.x509.encode())[0])
   def getCN(self):
-    c = re.search('\/CN=([^\/$]*)', self.getSubject())
+    c = re.search(r'/CN=([^/$]*)', self.getSubject())
     if c: return c.group(1)
     return self.getSubject()
   def getStartDate(self):
     p = Popen(["openssl", "x509", "-noout", "-startdate"], stdout=PIPE, stdin=PIPE)
-    return self.cleanDate(p.communicate(input=self.x509)[0])
+    return self.cleanDate(p.communicate(input=self.x509.encode())[0])
   def getEndDate(self):
     p = Popen(["openssl", "x509", "-noout", "-enddate"], stdout=PIPE, stdin=PIPE)
-    return self.cleanDate(p.communicate(input=self.x509)[0])
+    return self.cleanDate(p.communicate(input=self.x509.encode())[0])
   def getTimeToExpire(self):
     return self.enddate - datetime.now()
   def isValid(self):
@@ -44,13 +44,15 @@ class Ssl:
     fmt = "%b %d %H:%M:%S %Y %Z"
     return datetime.strptime(self.cleanOutput(datestr), fmt)
   def cleanOutput(self, output):
+    if isinstance(output, bytes):
+        output = output.decode()
     return str(output[(output.index('=')+1):]).strip()
 
 def main():
   files = []            # list of files to process
   certs = []            # list of certificates found
   retval = 0            # exit code
-  if options.debug: print "in main()"
+  if options.debug: print ("in main()")
 
   if options.file or options.directory: 
     getFiles(files)
@@ -59,8 +61,8 @@ def main():
 
     if options.verbose or options.debug:
       for cert in certs:
-        if options.verbose: print cert.file + "[" + str(cert.index) + "]: " + str(cert.getTimeToExpire().days) + " " + cert.getCN()
-        if options.debug: print cert.x509
+        if options.verbose: print (cert.file + "[" + str(cert.index) + "]: " + str(cert.getTimeToExpire().days) + " " + cert.getCN())
+        if options.debug: print (cert.x509)
 
     if options.orion:
       certsExpired = []           # all expired certificates
@@ -73,8 +75,8 @@ def main():
       orionPrint(certsExpired, certsUnexpired)
 
   if options.scan:
-    print "scanning..."
-    print socket.gethostbyname(socket.gethostname())
+    print ("scanning...")
+    print (socket.gethostbyname(socket.gethostname()))
 
   return retval
 
@@ -87,7 +89,7 @@ def getLocalIPs():
 def getFiles(i):
   if options.directory:
     for d in options.directory:
-      if options.verbose: print "processing dir: " + d
+      if options.verbose: print ("processing dir: " + d)
       for root, subFolders, files in os.walk(d, followlinks=False):
         if options.exclude:
           for exclude in options.exclude:
@@ -104,65 +106,65 @@ def getFiles(i):
       i.append(f)
 
 def orionPrint(e, u, b=None):
-  print "Statistic.Certs: " + str(len(e) + len(u))
-  print "Statistic.ExpiredCerts: " + str(len(e))
-  print "Message.ExpiredCerts: " + str(e)
+  print ("Statistic.Certs: " + str(len(e) + len(u)))
+  print ("Statistic.ExpiredCerts: " + str(len(e)))
+  print ("Message.ExpiredCerts: " + str(e))
   if len(u) > 0:
     if options.within > 0:
       w = []
       for c in u:
         if c.getTimeToExpire().days <= options.within: w.append(c)
-      print "Statistic.ExpiringCerts: " + str(len(w))
-      print "Message.ExpiringCerts: " + str(w) + " will expire within " + str(options.within) + " days"
+      print ("Statistic.ExpiringCerts: " + str(len(w)))
+      print ("Message.ExpiringCerts: " + str(w) + " will expire within " + str(options.within) + " days")
     else:
-      print "Statistic.ExpiringCerts: -1"
-      print "Message.ExpiringCerts: No certs expiring within " + str(options.within) + " days"
-    print "Statistic.DaysUntilNextExpiration: " + str(u[0].getTimeToExpire().days)
-    print "Message.DaysUntilNextExpiration: " + str(u[0])
+      print ("Statistic.ExpiringCerts: -1")
+      print ("Message.ExpiringCerts: No certs expiring within " + str(options.within) + " days")
+    print ("Statistic.DaysUntilNextExpiration: " + str(u[0].getTimeToExpire().days))
+    print ("Message.DaysUntilNextExpiration: " + str(u[0]))
   elif len(u) == 0 and len(e) == 0:
-    print "Statistic.ExpiringCerts: 0"
-    print "Message.ExpiringCerts: No certs found."
+    print ("Statistic.ExpiringCerts: 0")
+    print ("Message.ExpiringCerts: No certs found.")
   else:
-    print "Statistic.ExpiringCerts: -1"
-    print "Message.ExpiringCerts: All certs are expired!"
-    print "Statistic.DaysUntilNextExpiration: -1"
-    print "Message.DaysUntilNextExpiration: All certs are expired!"
+    print ("Statistic.ExpiringCerts: -1")
+    print ("Message.ExpiringCerts: All certs are expired!")
+    print ("Statistic.DaysUntilNextExpiration: -1")
+    print ("Message.DaysUntilNextExpiration: All certs are expired!")
   if not b is None and not (options.file or options.directory):
     if len(b) > 0:
-      print "Statistic.UnverifiedCerts: " + str(len(b))
-      print "Message.UnverifiedCerts: " + str(b)
+      print ("Statistic.UnverifiedCerts: " + str(len(b)))
+      print ("Message.UnverifiedCerts: " + str(b))
     else:
-      print "Statistic.UnverifiedCerts: -1"
-      print "Message.UnverifiedCerts: All certs chains are tested"
+      print ("Statistic.UnverifiedCerts: -1")
+      print ("Message.UnverifiedCerts: All certs chains are tested")
   if options.debug_orion:
-    print "Expired Certificates:"
-    for c in e: print c.getTimeToExpire().days, c
-    print "Unexpired Certificates:"
-    for c in u: print c.getTimeToExpire().days, c
-    print "Bad Certificate Chains:"
-    for c in b: print c
+    print ("Expired Certificates:")
+    for c in e: print (c.getTimeToExpire().days, c)
+    print ("Unexpired Certificates:")
+    for c in u: print (c.getTimeToExpire().days, c)
+    print ("Bad Certificate Chains:")
+    for c in b: print (c)
   return 0
 
 def orionProcess(c, e, u):
   for i in c:
-    if options.debug_orion or options.debug: print "orionProcess(): " + str(i.getSubject())
+    if options.debug_orion or options.debug: print ("orionProcess(): " + str(i.getSubject()))
     if i.isValid():
-      if options.debug_orion or options.debug: print "orionProcess(): " + str(i.isValid())
+      if options.debug_orion or options.debug: print ("orionProcess(): " + str(i.isValid()))
       u.append(i)
     else:
-      if options.debug_orion or options.debug: print "orionProcess(): " +str(i.isValid())
+      if options.debug_orion or options.debug: print ("orionProcess(): " +str(i.isValid()))
       e.append(i)
-  if options.verbose or ( options.debug_orion or options.debug): print "orionProcess():", str(len(c)), str(len(u)), str(len(e))
+  if options.verbose or ( options.debug_orion or options.debug): print ("orionProcess():", str(len(c)), str(len(u)), str(len(e)))
 
 def processFile(f):
   'Return a list of Certificate Blocks from an input file'
-  if options.debug: print "in processFile()"
-  if options.verbose: print "processing file: " + f
+  if options.debug: print ("in processFile()")
+  if options.verbose: print ("processing file: " + f)
   filecontents = open(f).read()
-  if options.debug: print "filehandle: " + filecontents
+  if options.debug: print ("filehandle: " + filecontents)
   c = re.findall("(-----BEGIN CERTIFICATE-----.*?-----END CERTIFICATE-----)", filecontents, re.DOTALL)
-  if options.debug: print "  certs: " + str(c)
-  if options.verbose: print "count: " + str(len(c))
+  if options.debug: print ("  certs: " + str(c))
+  if options.verbose: print ("count: " + str(len(c)))
   return c
 
 if __name__ == '__main__':
@@ -193,6 +195,6 @@ if __name__ == '__main__':
   parser.add_option_group(group)
   (options, args) = parser.parse_args()
   if options.debug: options.verbose=True
-  if options.debug: print "options " + str(options)
-  if options.debug: print "args " + str(args)
+  if options.debug: print ("options " + str(options))
+  if options.debug: print ("args " + str(args))
   sys.exit(main())
